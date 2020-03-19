8db47c47488aceb9e872b6e3f8e91a5f6de588d4
DianaKravtsova <di.kravts@mail.ru>
2020-03-19 17:43:16 +0300

    vova

--- untitled5/settings.py
+++ untitled5/settings.py
@@ -134,3 +134,6 @@ MEDIA_URL = '/media/'
 
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
